e2fbe52e1c44e7310da56a2332479763335ee45c
Firebok Playr <39416877+firebok2018@users.noreply.github.com>
2021-09-27 20:28:32 +0000

    df

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
+const mySecret = process.env['TOKEN']
 const Discord = require('discord.js');
-const client = new Discord.client();
+const client = new Discord.Client();
 require('dotenv').config();
 
-const { Client.Collection, Guild} =require('discord.js');
+const { Client, Collection, Guild} =require('discord.js');
 const keepAlive = require('./server.js')
 let prefix ='x/'
 
@@ -13,4 +15,10 @@ client.on('message', (message) =>{
   let usuario = message.mentions.members.first()|| message.member;
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+
+  if(command === "ping"){
+    message.channel.send("sdf")
+  }
 });
+
+client.login("ODkyMTMxMjA4NzkwNjk1OTU3.YVIb7w.MT9KpzhqFSHyKubJRK8ChTVfu-g")
